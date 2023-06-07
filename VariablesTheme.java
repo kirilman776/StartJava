b417587663af22df1bad2cf3663ee3d5a6c8738f
kirilman776 <mandarina776@gmail.com>
2023-06-07 19:32:47 +0300

    Сделано 3 задание

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -19,13 +19,19 @@ public class VariablesTheme {
         System.out.println("Операционная система: " + os);
         System.out.println("Русская клавиатура: " + russiankeybord);
 
-        System.out.println("2.Расчет стоимости товара со скидкой.");
+        System.out.println("\n2.Расчет стоимости товара со скидкой.");
         short pen = 100;
         short pencil = 200;
         float discount = 0.11F;
         System.out.println("Общая стоимость товаров без скидки = " + (pen + pencil));
         System.out.println("Cумма скидки = " + ((pen + pencil)*discount));
         System.out.println("Общая стоимость товаров со скидкой = " + ((pen + pencil)*(1 - discount)));
+
+        System.out.println("\n3.Вывод слова JAVA.");
+        System.out.println("   J    a  v     v  a   ");                                                  
+        System.out.println("   J   a a  v   v  a a  ");                                                 
+        System.out.println("J  J  aaaaa  V V  aaaaa ");                                                
+        System.out.println(" JJ  a     a  V  a     a");
 
 
     }
